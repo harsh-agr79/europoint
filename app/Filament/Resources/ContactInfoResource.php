4eed1d1ecb7e6abfdb465b61c10521a5b62f7633
harsh-agr79 <agrharsh7932@gmail.com>
2025-07-22 17:16:42 +0530

    Contact info

---
+++ app/Filament/Resources/ContactInfoResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactInfoResource\Pages;
+use App\Filament\Resources\ContactInfoResource\RelationManagers;
+use App\Models\ContactInfo;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+
+class ContactInfoResource extends Resource
+{
+    protected static ?string $model = ContactInfo::class;
+
+      protected static ?string $navigationIcon = 'heroicon-o-phone';
+
+    protected static ?string $navigationGroup = "Contact";
+
+    protected static ?int $navigationSort = 10;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+               TextInput::make('title')->disabled()->required(),
+                TextInput::make('value')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title'),
+                TextColumn::make('value'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    // Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+      public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContactInfos::route('/'),
+            // 'create' => Pages\CreateContactInfo::route('/create'),
+            // 'edit' => Pages\EditContactInfo::route('/{record}/edit'),
+        ];
+    }
+}
