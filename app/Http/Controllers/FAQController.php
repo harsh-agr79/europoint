cf74fa5885a3fad97a4d8086d22ef6ad47d73813
harsh-agr79 <agrharsh7932@gmail.com>
2025-07-24 22:42:34 +0530

    faqs tag

--- app/Http/Controllers/FAQController.php
+++ app/Http/Controllers/FAQController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\FAQ;
+use App\Models\MetaTag;
 
 class FAQController extends Controller
 {
     public function getFaq()
     {
         $faqs = FAQ::orderBy('order', 'desc')->get();
-        return response()->json($faqs);
+        return response()->json([
+            'faqs' => $faqs,
+            'meta_tags' => MetaTag::where('slug', 'faqs')->first()
+        ]);
     }
 }
