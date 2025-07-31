769368582a2c809be5048d1dad01fd4aaff0a4ee
harsh-agr79 <agrharsh7932@gmail.com>
2025-08-01 01:04:47 +0530

    Wishlist and apis

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wishlist extends Model
 {
-    //
+    protected $fillable = ['customer_id', 'tour_id'];
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class, 'tour_id');
+    }
 }
